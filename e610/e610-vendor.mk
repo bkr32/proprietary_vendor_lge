3baf2ddb6d35c89826c1e220558e1bd5f274039e
bkr32 <bkr3257@gmail.com>
2014-07-15 22:35:33 +0300

    Update e610-vendor.mk

--- e610/e610-vendor.mk
+++ e610/e610-vendor.mk
@@ -27,6 +27,6 @@ PRODUCT_COPY_FILES := \
     packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:/system/etc/permissions/android.software.live_wallpaper.xml
 
 # Pick up overlay for features that depend on non-open-source files
-DEVICE_PACKAGE_OVERLAYS := vendor/lge/e610/overlay
+DEVICE_PACKAGE_OVERLAYS := vendor/lge/e612/overlay
 
-$(call inherit-product, vendor/lge/e610/e610-vendor-blobs.mk)
+$(call inherit-product, vendor/lge/e612/e612-vendor-blobs.mk)
